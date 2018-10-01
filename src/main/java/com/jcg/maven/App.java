aefbfef9969a4e629f4ef513e4c9aecf727c97d0
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-10-01 10:18:23 +0200

    Adding new method with issues on feature 2 branch

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -10,4 +10,27 @@ public class App
     {
         System.out.println( "Hello World! How are you ?" );
     }
+
+    public void anotherMethodWithIssues() {
+        // Prepare metadata file
+        String metadataContent;
+        try {
+            System.out.println("a try catch statement");
+            // HashMap<String, String> ymlMap = stringYmlToMap(metadataContent);
+            // ymlMap.put("pop-name", epmService
+            // .getPopName(epmService.getRe().getHostIp(), "compose"));
+            // logger.info("New value for the field pop-name: {}",
+            // ymlMap.get("pop-name"));
+
+            // logger.info("New metadata content: {}",
+            // simpleYmlMapToString(ymlMap));
+            // filesService.writeFileFromString(simpleYmlMapToString(ymlMap),
+            // metadataFile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(
+                    "A runtime exception.");
+        }
+    }
 }
