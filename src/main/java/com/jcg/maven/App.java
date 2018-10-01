ddb22619c58861dbfae47ca3797f554d6d960d1c
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-10-01 10:44:06 +0200

    Adding new method with bugs

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -33,4 +33,12 @@ public class App
                     "A runtime exception.");
         }
     }
+
+    public void methodsWithBugs() {
+        int target = -5;
+        int num = 3;
+
+        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+        target =+ num; // Noncompliant; target = 3
+    }
 }
