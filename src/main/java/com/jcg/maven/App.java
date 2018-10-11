9683eeb3e6be4f8ec8ccdbc849d8842d6701bdfd
Aurelie Boiteux-Cabourdin <aurelie.boiteux@sonarsource.com>
2018-10-11 10:35:27 +0200

    Adding class with issues

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -15,4 +15,29 @@ public class App
         System.out.println("notCovered");
     }
 
+
+
+    public class Car{
+
+        public DriveTrain drive;
+
+        public void tearDown(){
+            System.out.println("Tear Down");
+        }
+
+        public void drive() {System.out.println("Drive");}  // Noncompliant; duplicates field name
+    }
+
+    public class MyCar extends Car{
+        public void teardown(){System.out.println("Tear Down");}  // Noncompliant; not an override. It it really what's intended?
+
+        public void drivefast(){System.out.println("Drive fast");}
+
+        public void driveFast(){System.out.println("I said: drive fast !");} //Huh?
+    }
+
+    public class DriveTrain{
+
+    }
+
 }
